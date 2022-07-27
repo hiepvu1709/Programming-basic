ace6654a7be8c364ca80da6430d54bdb83f324b5
Hiep <vuxuanhiep1709@gmail.com>
2022-07-27 08:25:08 +0700

    change

--- Front-End/JS/lesson-3-Array/Task05-Ex_Reduce.js
+++ Front-End/JS/lesson-3-Array/Task05-Ex_Reduce.js
@@ -2,5 +2,7 @@ const numbers = [1, 2, 3, 4, 5];
 const result = numbers.reduce((total, number) => {
     return total + number;
 }, 10);
+
+// output results: 25, bởi vì trong 
 console.log(result);
 
